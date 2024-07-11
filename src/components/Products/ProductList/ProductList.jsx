2c3f18343a0882569a600131adf3b9acc3284b6b
ozmberkan <ozmberkan@gmail.com>
2024-07-11 15:57:43 +0300

    v0.2 güncellemesi.

--- src/components/Products/ProductList/ProductList.jsx
+++ src/components/Products/ProductList/ProductList.jsx
@@ -11,14 +11,20 @@ const ProductList = ({ mainData, basket, setBasket, filtered }) => {
 
   return (
     <div className="w-full  grid grid-cols-4 content gap-10 py-5 px-36 mb-6">
-      {filteredData.map((product) => (
-        <Product
-          key={product.id}
-          product={product}
-          basket={basket}
-          setBasket={setBasket}
-        />
-      ))}
+      {filteredData.length > 0 ? (
+        filteredData.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            basket={basket}
+            setBasket={setBasket}
+          />
+        ))
+      ) : (
+        <p className="text-4xl font-bold">
+          Aradığınız Ürün Stoklarımızda Yer Almamaktadır!.
+        </p>
+      )}
     </div>
   );
 };
